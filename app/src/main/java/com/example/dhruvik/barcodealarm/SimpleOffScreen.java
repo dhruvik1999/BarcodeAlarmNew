60b5e514bb97ad1f6c42dd93cf2fd8618af4a65a
Dhruvik1999 <navadiyadhruvik2@gmail.com>
2018-11-09 04:22:14 -0800

    initial commit

--- app/src/main/java/com/example/dhruvik/barcodealarm/SimpleOffScreen.java
+++ app/src/main/java/com/example/dhruvik/barcodealarm/SimpleOffScreen.java
@@ -8,23 +8,31 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class SimpleOffScreen extends AppCompatActivity {
 
 
     Button stop,snooz;
-    TextView  showTime;
+    TextView  showTime,qus;
+    EditText uans;
     Calendar calNow,calAlarm;
+    Button check;
+    Random random;
+    int counter = 0;
+
 
     long timeLmit = 10 * 60 * 1000;
     long timeInterval = 1000;
-    int tar;
+    int tar,rans;
     int RequestCode;
 static boolean boundary = true;
 
@@ -37,6 +45,17 @@ static boolean boundary = true;
 Date date = new Date();
 calNow.setTime(date);
 calAlarm.setTime(date);
+
+check.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        String strans = uans.getText().toString();
+        if(checkAns(strans)){
+            setNext();
+            counter++;
+        }
+    }
+});
 
 tar = calNow.get(Calendar.HOUR_OF_DAY) * 100 + calNow.get(Calendar.MINUTE);
 
@@ -72,8 +91,6 @@ tar = calNow.get(Calendar.HOUR_OF_DAY) * 100 + calNow.get(Calendar.MINUTE);
 
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(),"UTHO JAGO AND DHYEY PRAPTI SUDHI MANDYA RAHO",Toast.LENGTH_SHORT).show();
-                //code for snoozinfg the alarm..
 
                 AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
@@ -94,11 +111,62 @@ tar = calNow.get(Calendar.HOUR_OF_DAY) * 100 + calNow.get(Calendar.MINUTE);
         });
     }
 
+    private void setNumber(){
+        snooz.setVisibility(View.GONE);
+        stop.setVisibility(View.GONE);
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+
+        rans = num1+num2;
+
+        qus.setText(Integer.toString(num1) + " " + "+" + " " + Integer.toString(num2));
+
+    }
+
+    private boolean checkAns(String uuans){
+        int checkAns;
+        try{
+            checkAns = Integer.parseInt(uuans);
+             if(checkAns == rans){
+                 Toast.makeText(getApplicationContext(),"Answer is Right...",Toast.LENGTH_LONG).show();
+
+             }else{
+                 Toast.makeText(getApplicationContext(),"Answer is Wrong...",Toast.LENGTH_LONG).show();
+             }
+            View view = this.getCurrentFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+            uans.setText(" ");
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(),"Please enter valid input : " + uuans,Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
+    private void setNext(){
+        if(counter < 3){
+           setNumber();
+        }else{
+            snooz.setVisibility(View.VISIBLE);
+            stop.setVisibility(View.VISIBLE);
+        }
+    }
+
+
     private void initiallixation(){
         stop = (Button)this.findViewById(R.id.stop);
         snooz = (Button)this.findViewById(R.id.snooz);
         showTime = (TextView)this.findViewById(R.id.showTime);
         calNow = Calendar.getInstance();
         calAlarm = Calendar.getInstance();
+        qus=(TextView)this.findViewById(R.id.question);
+        uans=(EditText)this.findViewById(R.id.ans);
+        check=(Button)this.findViewById(R.id.check);
+        random=new Random();
+        setNext();
+        snooz.setVisibility(View.GONE);
+        stop.setVisibility(View.GONE);
     }
 }
